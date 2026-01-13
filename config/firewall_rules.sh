52c41968b7f4d870315c25b2a7156548f0a1e9c9
DenisTheUkrainian <iamdenismoldovan@gmail.com>
2026-01-13 19:38:20 +0100

    Revert "testy v2"
    This reverts commit 13e5d83a4cb92c249e2b0cc015c0e4a5e14300fd.

--- config/firewall_rules.sh
+++ config/firewall_rules.sh
@@ -1,5 +1,6 @@
 
 WAN_IFACE=$(ip route show default | awk '/default/ {print $5}' | head -n 1)
+
 if [ -z "$WAN_IFACE" ]; then
     WAN_IFACE=$(ip -o -4 addr show | grep "172.20.0" | awk '{print $2}' | head -n 1)
 fi
@@ -13,7 +14,6 @@ NET_MGMT="10.0.99.0/24"
 IP_DNS="10.0.10.10"
 IP_WEB="10.0.20.10"
 IP_SIEM="10.0.99.10"
-IP_SCANNER="10.0.99.20"
 
 iptables -F
 iptables -X
@@ -23,35 +23,39 @@ iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
 
+
 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 
 iptables -A FORWARD -s $NET_BLUE -d $NET_GREEN -j DROP
 
-iptables -A FORWARD -s $IP_SCANNER -j ACCEPT
-
-iptables -A FORWARD -d $IP_SIEM -p udp --dport 514 -j ACCEPT
-iptables -A FORWARD -d $IP_SIEM -p tcp --dport 1514 -j ACCEPT
-
-iptables -A FORWARD -d $IP_DNS -p udp --dport 53 -j ACCEPT
-iptables -A FORWARD -d $IP_DNS -p tcp --dport 53 -j ACCEPT
-
 iptables -t nat -A POSTROUTING -o $WAN_IFACE -j MASQUERADE
-
 iptables -t nat -A PREROUTING -i $WAN_IFACE -p tcp --dport 80 -j DNAT --to-destination $IP_WEB:80
-iptables -A FORWARD -i $WAN_IFACE -d $NET_BLUE -p tcp -m multiport --dports 80,443 -j ACCEPT
+
+iptables -A FORWARD -s $NET_GREEN -d $IP_DNS -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -s $NET_GREEN -d $IP_DNS -p tcp --dport 53 -j ACCEPT
+
+iptables -A FORWARD -s $IP_DNS -o $WAN_IFACE -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -s $IP_DNS -o $WAN_IFACE -p tcp --dport 53 -j ACCEPT
+
 iptables -A FORWARD -i $WAN_IFACE -d $IP_DNS -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -i $WAN_IFACE -d $IP_DNS -p tcp --dport 53 -j ACCEPT
 
 iptables -A FORWARD -s $NET_GREEN -o $WAN_IFACE -j ACCEPT
-iptables -A FORWARD -s $NET_YELLOW -o $WAN_IFACE -j ACCEPT
-iptables -A FORWARD -s $NET_BLUE -o $WAN_IFACE -j ACCEPT
 
+iptables -A FORWARD -s $NET_YELLOW -o $WAN_IFACE -j ACCEPT
+
+iptables -A FORWARD -i $WAN_IFACE -d $NET_BLUE -p tcp -m multiport --dports 80,443 -j ACCEPT
 
 iptables -A FORWARD -s $NET_GREEN -d $NET_BLUE -p tcp -m multiport --dports 80,443 -j ACCEPT
 
 iptables -A FORWARD -s $NET_BLUE -d $NET_RED -p tcp --dport 5432 -j ACCEPT
 
-iptables -A FORWARD -s $NET_GREEN -d $NET_YELLOW -p icmp -j ACCEPT
+iptables -A FORWARD -s 10.0.99.20 -j ACCEPT
+
+iptables -A FORWARD -d $IP_SIEM -p udp --dport 514 -j ACCEPT
+iptables -A FORWARD -d $IP_SIEM -p tcp --dport 1514 -j ACCEPT
 
 iptables -A FORWARD -m limit --limit 5/min -j LOG --log-prefix "FW-DROP: " --log-level 4
+
